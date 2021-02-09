f749200c533e4dfa2f2d04500c163fe6b4f57f03
Ankit Somani <ankit.m.somani@gmail.com>
2021-02-08 20:38:04 -0500

    html ids

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+var lotrQuiz = document.getElementById("quiz");
+var lotrSumbit = document.getElementById("submit");
+var lotrScore = document.getElementById("score");
+
+function makeQuiz(){}
+var lotrQuestions = [
+    {
+        question: "Who is the director of The Lord of the Rings trilogy?",
+        answers: {
+            a: "Steven Spielberg",
+            b: "Michael Bay",
+            c: "Peter Jackson",
+            d: "Robert Zemeckis"
+        },
+        rightAnswer: "c"
+    },
+    {
+        questions: "Where do The Hobbits live?",
+        answers: {
+            a: "The Shire",
+            b: "The Mire",
+            c: "The Fire",
+            d: "The Dire"
+        },
+        rightAnswer: "a"
+    },
+    {
+        question: "Gimli, son of _____",
+        answers: {
+            a: 
+        }
+    }
+]
+
+
+function showScore(){}
+
+makeQuiz();
+
+lotrScore.addEventListener("click", showScore);
