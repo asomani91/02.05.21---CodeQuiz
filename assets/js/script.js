23ea573ab4ae1573f619a8e7f43aa91be6927249
Ankit Somani <ankit.m.somani@gmail.com>
2021-02-10 21:54:50 -0500

    timer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,79 +1,12 @@
 var lotrQuiz = document.getElementById("quiz");
 var lotrSumbit = document.getElementById("submit");
 var lotrScore = document.getElementById("score");
+var startButton = document.getElementById("start");
+var timeDisplay = document.getElementById("time");
+var timeLeft = 60;
 
 function makeQuiz() {
     var lotrQuestions = [
-        {
-            question: "Who is the director of The Lord of the Rings trilogy?",
-            answers: {
-                a: "Steven Spielberg",
-                b: "Michael Bay",
-                c: "Peter Jackson",
-                d: "Robert Zemeckis"
-            },
-            rightAnswer: "c"
-        },
-        {
-            question: "Where do The Hobbits live?",
-            answers: {
-                a: "The Shire",
-                b: "The Mire",
-                c: "The Fire",
-                d: "The Dire"
-            },
-            rightAnswer: "a"
-        },
-        {
-            question: "Gimli, son of _____.",
-            answers: {
-                a: "Thorin",
-                b: "Gloin",
-                c: "Dwalin",
-                d: "Balin"
-            },
-            rightAnswer: "b"
-        },
-        {
-            question: "Who is Frod Baggins related to?",
-            answers: {
-                a: "Bilbo",
-                b: "Merry",
-                c: "Pippin",
-                d: "Samwise"
-            },
-            rightAnswer: "a"
-        },
-        {
-            question: "Aragon is the heir of _______.",
-            answers: {
-                a: "Rohan",
-                b: "Elrond",
-                c: "Elendil",
-                d: "Isildur"
-            },
-            rightAnswer: "d"
-        },
-        {
-            question: "Who is the composer of The Lord of the Rings trilogy?",
-            answers: {
-                a: "John Williams",
-                b: "Hanz Zimmer",
-                c: "Howard Shore",
-                d: "Danny Elfman"
-            },
-            rightAnswer: "c"
-        },
-        {
-            question: "What does Aragorn say right before charging into the last battle in Mordor?",
-            answers: {
-                a: "Death",
-                b: "For Frodo",
-                c: "It Is Not This Day",
-                d: "You Look Awful"
-            },
-            rightAnswer: "b"
-        },
         {
             question: "How many Rings of Power are in existence?",
             answers: {
@@ -81,16 +14,6 @@ function makeQuiz() {
                 b: "9",
                 c: "10",
                 d: "20"
-            },
-            rightAnswer: "d"
-        },
-        {
-            question: "Fool of a ____!",
-            answers: {
-                a: "Nook",
-                b: "Fook",
-                c: "Took",
-                d: "Book"
             },
             rightAnswer: "d"
         },
@@ -104,13 +27,54 @@ function makeQuiz() {
             },
             rightAnswer: "b"
         },
+        {
+            question: "Gimli, son of _____.",
+            answers: {
+                a: "Thorin",
+                b: "Gloin",
+                c: "Dwalin",
+                d: "Balin"
+            },
+            rightAnswer: "b"
+        },
+        {
+            question: "Aragon is the heir of _______.",
+            answers: {
+                a: "Rohan",
+                b: "Elrond",
+                c: "Elendil",
+                d: "Isildur"
+            },
+            rightAnswer: "d"
+        },
+        {
+            question: "What does Aragorn say right before charging into the last battle in Mordor?",
+            answers: {
+                a: "Death",
+                b: "For Frodo",
+                c: "It Is Not This Day",
+                d: "You Look Awful"
+            },
+            rightAnswer: "b"
+        },
     ]
-},
+};
 
+function countDown() {
+    console.log("clicked")
+    var time = setInterval(function () {
+        timeLeft -= 1
+        timeDisplay.innerHTML = timeLeft
+        if (timeLeft <= 0) {
+            clearInterval(time)
+        }
+    }, 1000);
+}
 
+startButton.addEventListener("click", countDown)
 
 function showScore() { }
 
 makeQuiz();
 
-lotrScore.addEventListener("click", showScore);
+//lotrScore.addEventListener("click", showScore);
